4f0b5b7605ad029d32e32fd207b84f76306a2c4c
likhithhhh <likhithchappati@gmail.com>
2025-12-14 19:57:49 +0530

    fixed the url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const baseURL = "https://taskflow-backend-253z.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: baseURL, 
